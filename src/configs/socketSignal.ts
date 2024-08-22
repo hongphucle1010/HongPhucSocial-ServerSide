fb36c2de380472ae6ca8b405dc389d28c4e97ee2
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2024-08-22 17:48:23 +0700

    fix: fix socket.io leave room

---
+++ src/configs/socketSignal.ts
@@ -0,0 +1,9 @@
+// ======== Emit signal ========
+export const SOCKET_SEND_MESSAGE = "sendMessage";
+export const SOCKET_JOIN_MESSAGE_ROOM = "joinMessageRoom";
+export const SOCKET_JOIN_MESSAGE_LIST_ROOM = "joinMessageListRoom";
+export const SOCKET_LEAVE_MESSAGE_ROOM = "leaveMessageRoom";
+export const SOCKET_LEAVE_MESSAGE_LIST_ROOM = "leaveMessageListRoom";
+
+// ======== Receive signal ========
+export const SOCKET_RECEIVE_MESSAGE = "receiveMessage";
--- src/sockets/chat.ts
+++ src/sockets/chat.ts
@@ -1,5 +1,13 @@
 import { Server, Socket } from "socket.io";
 import { createMessage } from "../model/Message";
+import {
+  SOCKET_JOIN_MESSAGE_LIST_ROOM,
+  SOCKET_JOIN_MESSAGE_ROOM,
+  SOCKET_LEAVE_MESSAGE_LIST_ROOM,
+  SOCKET_LEAVE_MESSAGE_ROOM,
+  SOCKET_RECEIVE_MESSAGE,
+  SOCKET_SEND_MESSAGE,
+} from "../configs/socketSignal";
 
 export const getRoomName = (userId1: number, userId2: number) => {
   // Ensure user IDs are strings
@@ -17,25 +25,43 @@ const chatSocket = (socket: Socket, io: Server) => {
   console.log("A user connected: ", socket.id);
 
   // Join a specific chat room
-  socket.on("joinMessageRoom", (room) => {
+  socket.on(SOCKET_JOIN_MESSAGE_ROOM, (room) => {
     socket.join(room);
     console.log(`$Someone has joined room: ${room}`);
   });
 
+  socket.on(SOCKET_LEAVE_MESSAGE_ROOM, (room) => {
+    socket.leave(room);
+    console.log(`$Someone has left room: ${room}`);
+  });
+
+  socket.on(SOCKET_JOIN_MESSAGE_LIST_ROOM, (room) => {
+    socket.join(room);
+    console.log(`$Someone has joined room: ${room}`);
+  });
+
+  socket.on(SOCKET_LEAVE_MESSAGE_LIST_ROOM, (room) => {
+    socket.leave(room);
+    console.log(`$Someone has left room: ${room}`);
+  });
+
   // Listen for chat messages
-  socket.on("sendMessage", async ({ message, senderId, receiverId }) => {
+  socket.on(SOCKET_SEND_MESSAGE, async ({ message, senderId, receiverId }) => {
     const response = await createMessage({
       senderId,
       receiverId,
       content: message,
     });
-    io.to(getRoomName(senderId, receiverId)).emit("getMessage", {
-      id: 0,
-      senderId,
-      receiverId,
-      content: message,
-      createdAt: new Date().toISOString(),
-    });
+    io.to([getRoomName(senderId, receiverId), `list_${receiverId}`]).emit(
+      SOCKET_RECEIVE_MESSAGE,
+      {
+        id: 0,
+        senderId,
+        receiverId,
+        content: message,
+        createdAt: new Date().toISOString(),
+      }
+    );
   });
 
   socket.on("disconnect", () => {
